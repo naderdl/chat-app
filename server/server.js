b4847d13375f694f90f72f4aae7b8c2bb7bd1d99
Nader Daliri <naderdaliri@gmail.com>
2019-05-02 02:46:42 +0430

    initial commit

--- server/server.js
+++ server/server.js
@@ -2,12 +2,12 @@ const path = require('path');
 const express = require('express');
 
 const publicPath = path.join(__dirname, '../public');
-const PORT = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(express.static(publicPath));
 
-app.listen(PORT, () => {
-  console.log(`Here comes the magic: http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`Here comes the magic: http://localhost:${port}`);
 });
